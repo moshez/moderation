522a0ac7e3667490853f003c826e5ada39bceeec
Moshe Zadka <zadka.moshe@gmail.com>
2020-10-10 10:57:33 -0700

    +skeletal docs

---
+++ doc/conf.py
@@ -0,0 +1,6 @@
+extensions = []
+master_doc = "index"
+project = "Moderation"
+copyright = "2020, Moshe Zadka"
+author = "Moshe Zadka"
+version = release = "TBD"
---
+++ doc/index.rst
@@ -0,0 +1,4 @@
+Moderation
+==========
+
+This will be better.
